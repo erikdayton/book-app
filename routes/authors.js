2a68540df0bb410a56c4eb7492dd0ba4f1ba2a2a
Erik Dayton <erik@erikdayton.dev>
2022-06-16 09:56:41 -0500

    small edits

--- routes/authors.js
+++ routes/authors.js
@@ -3,17 +3,17 @@ const router = express.Router()
 const Author = require('../models/author')
 
 //All Authors Route
-
 router.get('/', async (req, res) => {
     let searchOptions = {}
-    if (req.query.name !== null && req.query.name !== '') {
+    if (req.query.name != null && req.query.name !== '') {
         searchOptions.name = new RegExp(req.query.name, 'i')
     }
     try {
         const authors = await Author.find(searchOptions)
         res.render('authors/index', {
             authors: authors,
-            searchOptions: req.query })
+            searchOptions: req.query 
+        })
     } catch {
         res.redirect('/')
     }
