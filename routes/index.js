4be1e5066bab9179804bf78ea038dedd3d08c234
Erik Dayton <erik@erikdayton.dev>
2022-07-11 09:12:44 -0500

    reformat book grid

--- routes/index.js
+++ routes/index.js
@@ -12,16 +12,5 @@ router.get('/', async (req, res) => {
     res.render('index', {books: books, user: req.user})
    
 })
-// router.get('/', async (req, res) => {
-//     let books
-//     try {
-//         books = await Book.find().sort({ createdAt: 'desc'}).limit(10).exec()
-//     } catch {
-//         books = []
-//     }
-//     res.render('indexWelcome', {books: books})
-// })
-
-
 
 module.exports = router
